91b517fe725b395e065dba2fa6d432f2cb5041b8
anmisttt <111nnn75@gmail.com>
2021-03-21 03:37:17 +0300

    videoplayer

--- src/components/film-card/film-card.jsx
+++ src/components/film-card/film-card.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import PropTypes from 'prop-types';
+import VideoPlayer from '../videoplayer/videoplayer';
 
 const FilmCard = (props) => {
   const {film, id} = props;
@@ -8,12 +9,12 @@ const FilmCard = (props) => {
     <>
       <article className="small-movie-card catalog__movies-card" onMouseEnter={() => setActive(id)}>
         <div className="small-movie-card__image">
-          <img
-            src={film.previewImage}
-            alt={film.name}
+          <VideoPlayer
+            src={film.previewVideoLink}
+            image={film.previewImage}
             width="280"
-            height="175"
-          />
+            height="175">
+          </VideoPlayer>
         </div>
         <h3 className="small-movie-card__title">
           <a className="small-movie-card__link" href="movie-page.html">
@@ -28,6 +29,7 @@ const FilmCard = (props) => {
 FilmCard.propTypes = {
   film: PropTypes.shape({
     name: PropTypes.string.isRequired,
+    previewVideoLink: PropTypes.string.isRequired,
     previewImage: PropTypes.string.isRequired,
   }),
   id: PropTypes.number.isRequired
